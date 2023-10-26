0ecf71ec2df62e435d707a50787d8a39074f8895
FouHamster <alina63111@gmail.com>
2023-10-26 13:14:36 +0700

    dhsajdhjka

--- js/main.js
+++ js/main.js
@@ -1,34 +1,22 @@
-// function fool(n){
-//     while (n == false){
-//         if (n == false) {
-//             alert('Введите число!')
-//             let n = parseInt(prompt('Введите радиус основания'))
-//         }
-//     }
-// }
+function fool(message){
+    while(true){
+        let data = parseInt(prompt(message))
 
+        if (isNaN(data)) {
+            alert('Введите число!!!')
+            continue;
+        }
+    
+        return data;
+    }
+}
 // задание 1
-let r = parseInt(prompt('Введите радиус основания'))
-if (isNaN(r)) {
-    alert('Нужно писать число!')
-} 
-
-let h = parseInt(prompt('Введите высоту конуса'))
-if (isNaN(h)) {
-    alert('Нужно писать число!')
-}
-
-let l = parseInt(prompt('Введите образующую конуса'))
-if (isNaN(l)) {
-    alert('Нужно писать число!')
-}
+let r = fool("Введите радиус конуса")
+let h = fool('Введите высоту конуса')
+let l = fool('Введите образующую конуса')
 
 let v = 3 / (Math.PI * (r ** 2) * h)
 let s = Math.PI * r * l
-
-// if ((v || s) == NaN){
-//         alert('Введите числа!!!')
-// }
 
 console.log('Объём конуса: ', v)
 console.log('Площадь боковой поверхности: ', s)
@@ -39,15 +27,8 @@ document.getElementById('ploshad').innerHTML = `Площадь боковой п
 // задание 2
 alert('ax + b = 0')
 
-let a = parseInt(prompt('Введите значение а:'))
-if (isNaN(a)) {
-    alert('Нужно писать число!')
-}
-
-let b = parseInt(prompt('Введите значение b:'))
-if (isNaN(b)) {
-    alert('Нужно писать число!')
-}
+let a = fool('Введите значение а:')
+let b = fool('Введите значение b:')
 
 let x = b / a
 
@@ -57,11 +38,12 @@ document.getElementById('yraw').innerHTML = `Значение x = ${x}`
 // Задание 3
 let p = 0
 n = 1
+
 for (let i = 1; n <= 10; i+=2) {
-        let p = ((Math.PI * (i ** 2)).toFixed(2))
-        // document.getElementById('pl').innerHTML = `Круг ${n} = ${p}`
-        console.log(`Круг ${n} = ${p}`)  
-        n += 1       
+    let p = ((Math.PI * (i ** 2)).toFixed(2))
+    // document.getElementById('pl').innerHTML = `Круг ${n} = ${p}`
+    console.log(`Круг ${n} = ${p}`)  
+    n += 1       
 } 
 
 
